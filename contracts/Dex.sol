a9bbb73929f8381be163dffa198d1809a752a250
5ecurealf <alfielloyd12@gmail.com>
2023-07-15 23:47:16 +0100

    DEX contract that will allow a user to sell any ERC20 contract

---
+++ contracts/Dex.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DEX {
+    IERC20 public associatedToken;
+
+    uint price;
+    address owner;
+
+    constructor(IERC20 _token, uint _price) {
+        associatedToken = _token;
+        price = _price;
+        owner = msg.sender;
+    }
+}
