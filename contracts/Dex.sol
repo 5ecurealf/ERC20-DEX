66b2d85329f120490322f3a0aff22647fdf276df
5ecurealf <alfielloyd12@gmail.com>
2023-07-15 23:59:13 +0100

    owner of the tokens allows DEX to sell coins

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -13,4 +13,24 @@ contract DEX {
         price = _price;
         owner = msg.sender;
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "you are not the owner");
+        _;
+    }
+
+    // check if the owner of this contract has given an allowance to the other contract
+    function sell() external onlyOwner {
+        uint allowance = associatedToken.allowance(msg.sender, address(this));
+        require(
+            allowance > 0,
+            "you must allow this contract an allowance of at least one token"
+        );
+        bool sent = associatedToken.transferFrom(
+            msg.sender,
+            address(this),
+            allowance
+        );
+        require(sent, "Failed to send");
+    }
 }
